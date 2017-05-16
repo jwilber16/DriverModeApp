418e5d3f904653e79d9722661ec33d4995262468
Josh <joshuawilber@sbcglobal.net>
2017-05-15 20:29:30 -0700

    -Added functionality to buttons on main menu

--- app/src/main/java/com/example/josh/drivermodeapp/DialerActivity.java
+++ app/src/main/java/com/example/josh/drivermodeapp/DialerActivity.java
@@ -3,6 +3,7 @@ package com.example.josh.drivermodeapp;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class DialerActivity extends AppCompatActivity {
 
@@ -10,6 +11,23 @@ public class DialerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialer_portion);
+
+        Button oneButton = (Button)findViewById(R.id.oneButton);
+        Button twoButton = (Button)findViewById(R.id.twoButton);
+        Button threeButton = (Button)findViewById(R.id.threeButton);
+        Button foutButton = (Button)findViewById(R.id.fourButton);
+        Button fiveButton = (Button)findViewById(R.id.fiveButton);
+        Button sixButton = (Button)findViewById(R.id.sixButton);
+        Button sevenButton = (Button)findViewById(R.id.sevenButton);
+        Button eightButton = (Button)findViewById(R.id.eightButton);
+        Button nineButton = (Button)findViewById(R.id.nineButton);
+        Button starButton = (Button)findViewById(R.id.starButton);
+        Button zeroButton = (Button)findViewById(R.id.zeroButton);
+        Button poundButton = (Button)findViewById(R.id.poundButton);
+        Button backButton = (Button)findViewById(R.id.backButton);
+        Button callButton = (Button)findViewById(R.id.callButton);
+        Button backspaceButton = (Button)findViewById(R.id.backspaceButton);
+        Button contactsButton = (Button)findViewById(R.id.contactsButton);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
--- app/src/main/java/com/example/josh/drivermodeapp/MainMenuFragment.java
+++ app/src/main/java/com/example/josh/drivermodeapp/MainMenuFragment.java
@@ -8,8 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.view.View.OnClickListener;
 
-public class MainMenuFragment extends Fragment implements View.OnClickListener {
+public class MainMenuFragment extends Fragment {
 
     @Nullable
     @Override
@@ -17,16 +18,38 @@ public class MainMenuFragment extends Fragment implements View.OnClickListener {
 
         View view  = inflater.inflate(R.layout.main_menu_fragment, container, false);
 
+        //Call button open dialer activity
         Button callButton = (Button)view.findViewById(R.id.phoneButton);
-        callButton.setOnClickListener(this);
+        callButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), DialerActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        //Media button open media activity
+        Button mediaButton = (Button)view.findViewById(R.id.mediaButton);
+        mediaButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), DialerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Navigation button opens navigation activity
+        Button navButton = (Button)view.findViewById(R.id.navButton);
+        navButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Open navigation activity
+            }
+        });
         return view;
     }
 
-    @Override
-    public void onClick(View v) {
-        Intent intent = new Intent(getContext(), DialerActivity.class);
-        startActivity(intent);
-    }
+
+
 
 }
